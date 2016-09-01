6ee8720bec56a792dee570c7f71fa9a338dd36b9
whitney-larsen <whitney.larsen2@gmail.com>
2016-08-31 21:16:18 -0700

    Update contact_manager.py

--- contact_manager.py
+++ contact_manager.py
@@ -70,18 +70,13 @@ def edit_contact():
 					return
 	print "Contact Not Found."
 
+
 def write_file(file_name):
-	contact_text_list =[]
-	for i in range(len(contact_list)):
-		contact_text_list[i][0] = contact_list[i].first_name
-		contact_text_list[i][1] = contact_list[i].last_name
-		contact_text_list[i][2] = contact_list[i].mobile
-
-
+	contact_string = ""
+	for contact in contact_list:
+		contact_string = contact_string + contact.first_name + "," + contact.last_name + "," + contact.mobile_number + "," + contact.work_number + "," + contact.email
 	with open (file_name, mode = 'a') as my_file:
-		my_file.write(contact_text_list)
-
-
+		my_file.write(contact_string)
 
 # def main():
 # 	add_new_contact()
